d2bc3567cb54c3f511d482b8f3f426b68a0c1956
serifagir <serifagir64@gmail.com>
2023-04-02 17:20:08 +0300

    coming soon page

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className="main-div">
+        <h1>Aegire</h1>
+        <p>Coming soon...</p>
+    </div>
+  )
+}
+
+export default App;
